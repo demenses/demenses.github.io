48f323f0e46b24c911981387f6272d40f25d8873
Nico Hickman <nicohman@protonmail.com>
2017-12-30 12:16:41 -0800

    Add IP. Should now redirect to demenses.

--- re.js
+++ re.js
@@ -1,4 +1,5 @@
 window.onload = function(){
-	window.location.href = "IP";
-	document.getElementById("re").href = "http://IP"
+	var IP = "24.113.235.229:3953/index.html"
+	window.location.href = "http://"+IP;
+	document.getElementById("re").href = "http://"+IP;
 }
